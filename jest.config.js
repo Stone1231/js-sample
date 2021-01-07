471635225b4544483cd76ef1879051d8d247fed2
stone <stone.yang@nt-tech.tw>
2021-01-07 14:59:39 +0800

    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
   coverageDirectory: "coverage",
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(t|j)sx?$",
 };
